8ff3e35cc7c3db8550e35ae4e1a5e0b213f234de
unknown <szymkiewiczmateusz1@gmail.com>
2022-06-04 15:44:00 +0200

    help.php

--- help.php
+++ help.php
@@ -6,17 +6,91 @@
     <link rel="shortcut icon" href="res/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale" />
     <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="fontello-013dd3c1/css/fontello.css" type="text/css">
     <style>
         body{
             background: #208c71;
-            color: #fff;
             font-family: 'DotGothic16';
-            text-align: center;
-            padding-top: 20vh;
+            padding: 30px;
+        }
+        h1{
+            color: #fff;
+        }
+        ul.tree-view{-webkit-font-smoothing:none;}
+        ul.tree-view{background:#fff;box-shadow:inset -1px -1px #fff,inset 1px 1px grey,inset -2px -2px #dfdfdf,inset 2px 2px #0a0a0a;display:block;margin:0;padding:6px}ul.tree-view li{list-style-type:none}ul.tree-view li,ul.tree-view ul{margin-top:3px}ul.tree-view ul{border-left:1px dotted grey;margin-left:16px;padding-left:16px}ul.tree-view ul>li{position:relative}ul.tree-view ul>li:before{border-bottom:1px dotted grey;content:"";display:block;left:-16px;position:absolute;top:6px;width:12px}ul.tree-view ul>li:last-child:after{background:#fff;bottom:0;content:"";display:block;left:-20px;position:absolute;top:7px;width:8px}ul.tree-view details{margin-top:0}ul.tree-view details[open] summary{margin-bottom:0}ul.tree-view ul details>summary:before{margin-left:-22px;position:relative;z-index:1}ul.tree-view details>summary:before{background-color:#fff;border:1px solid grey;content:"+";display:block;float:left;height: 13px;line-height:9px;margin-right:5px;padding-left:0.5px;text-align:center;width:12px}ul.tree-view details[open]>summary:before{content:"-"}ul.tree-view details>summary::-webkit-details-marker,ul.tree-view details>summary::marker{content:""}
+        li{
+            font-size: 18px;
+            outline: none;
+        }
+        summary{
+            outline: none;
+            cursor: pointer;
+        }
+        summary:before{
+            font-size: 10px;
+        }
+        ::selection{
+            background: #fff;
+            color: #208c71;
+        }
+        li::selection, summary::selection{
+            background: #969eff;
+            color: #fff;
+        }
+        i, span{
+            color: #fff;
+            font-size: 28px;
+        }
+         .back {
+            position: absolute;
+            top: 5px;
+            right: 24px;
+            transform: rotate(90deg);
         }
     </style>
     </head>
     <body>
-        <h1>Strona w budowie, musisz zaczekać ;)</h1>
+        <h1>Pomoc</h1>
+        <a href="index.php"><img src="res/back.png" width="60px" height="60px" class="back"></a>
+        <ul class="tree-view">
+  <li>FAQ</li>
+  <li>Najczęsciej zadawane pytania :</li>
+  <li>
+    <details>
+      <summary>Do czego służy kazachstan.net?</summary>
+      <ul>
+        <li>Kazachstan.net to zintegralizowany system służacy urzędom cywilnym na terenach całego kazachstanu służacy inwigilowaniu oraz kontrolowaniu obywateli kazachskich.</li>
+      </ul>
+    </details>
+  </li>
+<li>
+    <details>
+      <summary>Kto jest twórcą projektu?</summary>
+      <ul>
+        <li>Projekt został w całości zrealizowany oraz zaprogramowany przez programistów z wydziału cybernetyki na uniwersytecie im.Lenina w Karagandzie.</li>
+      </ul>
+    </details>
+  </li>
+    <li>
+    <details>
+      <summary>Co grozi mi za manipulowanie danymi?</summary>
+      <ul>
+        <li>Manipulowanie danymi z bazy danych kazachstan.net jest karane wysłaniem do obozu reformalizacyjnego lub do 10000000 rubli grzywny, zależnie od wyrządzonych szkód</li>
+      </ul>
+    </details>
+  </li>
+     <li>
+    <details>
+      <summary>Czy mogę zostać współtwórcą kazachstan.net?</summary>
+      <ul>
+        <li>Rekrutacja do zespołu kazachstan.net odbywa się na uniwersytecie im. Lenina w Karagandzie lub zdalnie, wystarczy wejść na strone naszego repozytorium na githubie.</li>
+      </ul>
+    </details>
+  </li>
+</ul>
+<h1>Kontakt</h1>
+<i class="icon-mail"></i><span> - kazachstan.net @ gmail.com</span><br />
+<i class="icon-info"></i><span> - Plac im. Lenina 12, Astana</span><br />
+<i class="icon-user"></i><span> - 974 347 263</span>
     </body>
 </html>
