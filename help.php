fb856fed9b327f2f65090678defcef0bf5b6d407
unknown <szymkiewiczmateusz1@gmail.com>
2022-06-04 19:54:17 +0200

    Style

--- help.php
+++ help.php
@@ -16,8 +16,7 @@
         h1{
             color: #fff;
         }
-        ul.tree-view{-webkit-font-smoothing:none;}
-        ul.tree-view{background:#fff;box-shadow:inset -1px -1px #fff,inset 1px 1px grey,inset -2px -2px #dfdfdf,inset 2px 2px #0a0a0a;display:block;margin:0;padding:6px}ul.tree-view li{list-style-type:none}ul.tree-view li,ul.tree-view ul{margin-top:3px}ul.tree-view ul{border-left:1px dotted grey;margin-left:16px;padding-left:16px}ul.tree-view ul>li{position:relative}ul.tree-view ul>li:before{border-bottom:1px dotted grey;content:"";display:block;left:-16px;position:absolute;top:6px;width:12px}ul.tree-view ul>li:last-child:after{background:#fff;bottom:0;content:"";display:block;left:-20px;position:absolute;top:7px;width:8px}ul.tree-view details{margin-top:0}ul.tree-view details[open] summary{margin-bottom:0}ul.tree-view ul details>summary:before{margin-left:-22px;position:relative;z-index:1}ul.tree-view details>summary:before{background-color:#fff;border:1px solid grey;content:"+";display:block;float:left;height: 13px;line-height:9px;margin-right:5px;padding-left:0.5px;text-align:center;width:12px}ul.tree-view details[open]>summary:before{content:"-"}ul.tree-view details>summary::-webkit-details-marker,ul.tree-view details>summary::marker{content:""}
+        ul.tree-view{background:#fff;box-shadow:inset -1px -1px #fff,inset 1px 1px grey,inset -2px -2px #dfdfdf,inset 2px 2px #0a0a0a;display:block;margin:0;padding:20px}ul.tree-view li{list-style-type:none}ul.tree-view li,ul.tree-view ul{margin-top:3px}ul.tree-view ul{border-left:1px dotted grey;margin-left:16px;padding-left:16px}ul.tree-view ul>li{position:relative}ul.tree-view ul>li:before{border-bottom:1px dotted grey;content:"";display:block;left:-16px;position:absolute;top:6px;width:12px}ul.tree-view ul>li:last-child:after{background:#fff;bottom:0;content:"";display:block;left:-20px;position:absolute;top:7px;width:8px}ul.tree-view details{margin-top:0}ul.tree-view details[open] summary{margin-bottom:0}ul.tree-view ul details>summary:before{margin-left:-22px;position:relative;z-index:1}ul.tree-view details>summary:before{background-color:#fff;border:1px solid grey;content:"+";display:block;float:left;height: 13px;line-height:9px;margin-right:5px;padding-left:0.5px;text-align:center;width:12px}ul.tree-view details[open]>summary:before{content:"-"}ul.tree-view details>summary::-webkit-details-marker,ul.tree-view details>summary::marker{content:""}
         li{
             font-size: 18px;
             outline: none;
@@ -40,6 +39,7 @@
         i, span{
             color: #fff;
             font-size: 28px;
+            letter-spacing: -1px;
         }
          .back {
             position: absolute;
@@ -57,15 +57,15 @@
   <li>Najczęsciej zadawane pytania :</li>
   <li>
     <details>
-      <summary>Do czego służy kazachstan.net?</summary>
+      <summary>Czym jest kazachstan.net?</summary>
       <ul>
-        <li>Kazachstan.net to zintegralizowany system służacy urzędom cywilnym na terenach całego kazachstanu służacy inwigilowaniu oraz kontrolowaniu obywateli kazachskich.</li>
+        <li>Kazachstan.net to zintegralizowany system służacy urzędom cywilnym na terenach całego kazachstanu do inwigilowania oraz kontrolowania naszych obywateli.</li>
       </ul>
     </details>
   </li>
 <li>
     <details>
-      <summary>Kto jest twórcą projektu?</summary>
+      <summary>Kto stworzył ten projekt?</summary>
       <ul>
         <li>Projekt został w całości zrealizowany oraz zaprogramowany przez programistów z wydziału cybernetyki na uniwersytecie im.Lenina w Karagandzie.</li>
       </ul>
@@ -90,7 +90,7 @@
 </ul>
 <h1>Kontakt</h1>
 <i class="icon-mail"></i><span> - kazachstan.net @ gmail.com</span><br />
-<i class="icon-info"></i><span> - Plac im. Lenina 12, Astana</span><br />
+<i class="icon-info"></i><span> - Plac Lenina 12, Astana</span><br />
 <i class="icon-user"></i><span> - 974 347 263</span>
     </body>
 </html>
